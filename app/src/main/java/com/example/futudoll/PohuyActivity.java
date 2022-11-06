911b7892322cef944c86f5b40f093ffeb12e6876
Udaka-Hayerson <mikehayerson@gmail.com>
2022-11-06 21:10:53 +0200

    adds work chronometer without saving results in file

--- app/src/main/java/com/example/futudoll/PohuyActivity.java
+++ app/src/main/java/com/example/futudoll/PohuyActivity.java
@@ -3,36 +3,45 @@ package com.example.futudoll;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.Timer;
 import java.util.TimerTask;
+
 
 public class PohuyActivity extends Activity {
 
     public TextView tvView;
     public TextView timerView;
     public Button btnBackReg;
-    public Button btnStartWorkTime;
+    Timer myTimer;
+    private Chronometer chronometer;
+    private long pause_off_set;
+    private boolean running;
     int start = 0;
+    int count = 0;
+    String fName;
+    String lName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pohuy);
 
+        chronometer = (Chronometer) findViewById(R.id.chronometer);
         tvView = (TextView) findViewById(R.id.tvView);
         timerView = (TextView) findViewById(R.id.timerView);
 
         Intent intent = getIntent();
 
-        String fName = intent.getStringExtra("fname");
-        String lName = intent.getStringExtra("lname");
+        fName = intent.getStringExtra("fname");
+        lName = intent.getStringExtra("lname");
 
-        tvView.setText("Your name is: " + fName + " " + lName);
+        tvView.setText(" " + fName + " " + lName + " works " + count + " seconds");
 
         btnBackReg = (Button) findViewById(R.id.btnBackReg);
         btnBackReg.setOnClickListener(new View.OnClickListener() {
@@ -45,52 +54,45 @@ public class PohuyActivity extends Activity {
             }
         });
 
-        btnStartWorkTime = (Button) findViewById(R.id.work_timer);
-        btnStartWorkTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new Timer().scheduleAtFixedRate(new TimerTask() {
-                    @Override
-                    public void run() {
-                        timerView.setText("" + (start++));
-                        //Log.i("tag", "A Kiss every 5 seconds");
-                    }
-
-                }, 0, 1);
-                //If the timer counts in seconds, the period must be equal to a thousand.
-                // adds a time counter in milliseconds
-
-
-            }
-        });
-    //timer variation
-        Timer myTimer;
-        myTimer = new Timer();
-
-        myTimer.schedule(new TimerTask() {
-            public void run() {
-                timerTick();
-            }
-        }, 0, 3000); // каждые 5 секунд
 
     }
-    private void timerTick () {
-            this.runOnUiThread(doTask);
+
+
+    public void startChronometer(View view) {
+        if(!running){
+            myTimer = new Timer();
+            myTimer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    timerView.setText("" + (start++));
+                }
+
+            }, 0, 1000);
+            chronometer.setBase(SystemClock.elapsedRealtime() - pause_off_set);
+            chronometer.start();
+            running = true;
         }
+    }
 
-    private Runnable doTask = new Runnable() {
-        public void run() {
-            Toast toast = Toast.makeText(getApplicationContext(), "3 секунды!",
-                    Toast.LENGTH_SHORT);
-            toast.show();
+    public void pauseChronometer(View view) {
+        if(running) {
+            myTimer.cancel();
+            count = count + start;
+            start = 0;
+            tvView.setText(" " + fName + " " + lName + " works " + count + " seconds");
+            chronometer.stop();
+            pause_off_set = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
         }
-    };
-    //timer variation
+    }
 
-
-
-
-
+    public void resetChronometer(View view) {
+        count = count + start;
+        start = 0;
+        tvView.setText(" " + fName + " " + lName + " works " + count + " seconds");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pause_off_set = 0;
+    }
 }
 
 //    public void saveData(View view) {
