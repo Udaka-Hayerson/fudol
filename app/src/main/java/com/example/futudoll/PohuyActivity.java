7fb019213b7f6b91fec4418b8dfff6fd81347ad5
Udaka-Hayerson <mikehayerson@gmail.com>
2022-11-17 18:01:18 +0200

    changes methods saveData and openData

--- app/src/main/java/com/example/futudoll/PohuyActivity.java
+++ app/src/main/java/com/example/futudoll/PohuyActivity.java
@@ -9,14 +9,19 @@ import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
 
 public class PohuyActivity extends Activity {
 
-    public TextView tvView;
+    public TextView first_last_name;
     public TextView timerView;
+    public TextView work_sec_counter;
     public Button btnBackReg;
     Timer myTimer;
     private Chronometer chronometer;
@@ -33,15 +38,18 @@ public class PohuyActivity extends Activity {
         setContentView(R.layout.activity_pohuy);
 
         chronometer = (Chronometer) findViewById(R.id.chronometer);
-        tvView = (TextView) findViewById(R.id.tvView);
+        chronometer.setFormat("Time: %s");
+        first_last_name = (TextView) findViewById(R.id.first_last_name);
         timerView = (TextView) findViewById(R.id.timerView);
+        work_sec_counter = (TextView) findViewById(R.id.work_sec_counter);
 
         Intent intent = getIntent();
 
         fName = intent.getStringExtra("fname");
         lName = intent.getStringExtra("lname");
 
-        tvView.setText(" " + fName + " " + lName + " works " + count + " seconds");
+        first_last_name.setText(" " + fName + " " + lName + " works (in seconds) :");
+        work_sec_counter.setText("" + count);
 
         btnBackReg = (Button) findViewById(R.id.btnBackReg);
         btnBackReg.setOnClickListener(new View.OnClickListener() {
@@ -79,7 +87,8 @@ public class PohuyActivity extends Activity {
             myTimer.cancel();
             count = count + start;
             start = 0;
-            tvView.setText(" " + fName + " " + lName + " works " + count + " seconds");
+            work_sec_counter.setText("" + count);
+            saveData(work_sec_counter); // потім змінити
             chronometer.stop();
             pause_off_set = SystemClock.elapsedRealtime() - chronometer.getBase();
             running = false;
@@ -89,10 +98,64 @@ public class PohuyActivity extends Activity {
     public void resetChronometer(View view) {
         count = count + start;
         start = 0;
-        tvView.setText(" " + fName + " " + lName + " works " + count + " seconds");
+        saveData(work_sec_counter); // потім змінити
+        work_sec_counter.setText("" + count);
         chronometer.setBase(SystemClock.elapsedRealtime());
         pause_off_set = 0;
     }
+
+    public void saveData(View view) {
+        timerView = findViewById(R.id.timerView);
+        FileOutputStream fileOutput = null;
+        try {
+            String myTxt = work_sec_counter.getText().toString();
+            fileOutput = openFileOutput("work_time.txt", MODE_PRIVATE); //MODE_APPEND
+            fileOutput.write(myTxt.getBytes());
+            Toast.makeText(PohuyActivity.this, "We are save \n your birthday", Toast.LENGTH_LONG).show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (fileOutput != null)
+                    fileOutput.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //TODO: потрібно зробити так щоб кожного разу в
+    // файл записувався сумма , з того що в ньому є
+    // і лічильника роботи в секундах за останній
+    // робочій період. Тобто метод openDate  має
+    // бути інтегрованим в метод saveDate
+
+    public void openData(View view) {
+        FileInputStream fileInput = null;
+        work_sec_counter = findViewById(R.id.work_sec_counter);
+        try {
+            fileInput = openFileInput("work_time.txt");
+            byte[] bytes = new byte[1024];
+            fileInput.read(bytes);
+            String text = new String(bytes);
+            work_sec_counter.setText(text);
+            fileInput.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fileInput != null)
+                    fileInput.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
 
 //    public void saveData(View view) {
@@ -103,7 +166,7 @@ public class PohuyActivity extends Activity {
 //            String myTxt = edit.getText().toString();
 //            fileOutput = openFileOutput(FILE_NAME, MODE_PRIVATE); //MODE_APPEND
 //            fileOutput.write(myTxt.getBytes());
-//            Toast.makeText(MainActivity.this, "We are save \n your birthday", Toast.LENGTH_LONG).show();
+//            Toast.makeText(PohuyActivity.this, "We are save \n your birthday", Toast.LENGTH_LONG).show();
 //        }
 //        catch (IOException e) {
 //            e.printStackTrace();
