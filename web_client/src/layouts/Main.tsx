a6ca5978126c9fe37719adf5e9a2e2bfca5148f7
bringmetheaugust <bringmetheaugust@gmail.com>
2024-10-07 20:17:49 +0300

    fix web_client docker

--- web_client/src/layouts/Main.tsx
+++ web_client/src/layouts/Main.tsx
@@ -28,12 +28,12 @@ const MainLayout = () => {
                         </ListItemIcon>
                         <ListItemText primary="users" />
                     </ListItemButton>
-                    <ListItemButton selected={pathname === "/fav"} onClick={() => handleChange("fav")}>
+                    {/* <ListItemButton selected={pathname === "/fav"} onClick={() => handleChange("fav")}>
                         <ListItemIcon>
                             <FavoriteIcon />
                         </ListItemIcon>
                         <ListItemText primary="Drafts" />
-                    </ListItemButton>
+                    </ListItemButton> */}
                 </List>
             </div>
             <main className="p-2 w-full">
