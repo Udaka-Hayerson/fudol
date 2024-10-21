a645779bd292a8dc9398605ce6c203be556894b1
Udaka-Hayerson <mikehayerson@gmail.com>
2024-10-21 17:52:22 +0300

    thank you August for https://mainbus.uno

--- client/app/src/main/java/com/example/futudoll/activities/Utils.java
+++ client/app/src/main/java/com/example/futudoll/activities/Utils.java
@@ -10,7 +10,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Utils {
-    public static MainApi retrofitInstance(Context context){
+    public static MainApi retrofitInstance(Context context) {
         Retrofit retrofit;
         MainApi mainApi;
         HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
@@ -18,7 +18,7 @@ public class Utils {
         OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
         httpClient.addInterceptor(logging);
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://10.0.2.2:8080/")
+                .baseUrl("https://mainbus.uno/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(httpClient.build())
                 .build();
