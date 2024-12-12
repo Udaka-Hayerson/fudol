4b7c4e190a7efca9492c605b94338b288df66011
bringmetheaugust <bringmetheaugust@gmail.com>
2024-12-12 21:53:29 +0200

    add parentID field to public data

--- api/models/todo.go
+++ api/models/todo.go
@@ -1,9 +1,12 @@
 package models
 
+import "fudol_api/schemas"
+
 type Todo struct {
-	ID          int    `bson:"_id" json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	List        []Todo `json:"list"`
-	IsCompleted bool   `json:"isCompleted"`
+	ID          int            `bson:"_id" json:"id"`
+	ParentID    schemas.TodoID `bson:"parentID,omitempty" json:"parentID"`
+	Title       string         `json:"title"`
+	Description string         `json:"description"`
+	List        []Todo         `json:"list"`
+	IsCompleted bool           `json:"isCompleted"`
 }
